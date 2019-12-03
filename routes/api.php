2b0b333fac7d15ab7fbfa050f90ce245e83c94a2
Naiguel Correa dos santos <naiguelsantos@iMac-de-Naiguel.local>
2019-12-03 17:13:11 -0300

    add bootstrap and vuejs components, list and add tasks

--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,6 @@
 
 use Illuminate\Http\Request;
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
 
 Route::get('tasks', 'TaskController@index');
 Route::get('task/{id}', 'TaskController@show');
